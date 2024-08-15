385fa3b323957b81ee85589288c9021ab318f393
Ankit( ManoffSteel) <110630262+Manoffsteel@users.noreply.github.com>
2024-08-15 23:38:42 +0530

    Update App.jsx
    now using fregments to remove extra dependency on div

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import React from "react";
+export const App =()=>{
+  return (
+    //react.frag use krne se ek extra div ka locha hat 
+    // jata ha  dusra uspye array and key bana kr
+    // short cut <> bs yhi use krlo diect
+    <React.Fragment>   
+
+    <img src="IMAGE 2024-06-12 13:58:22.jpg" alt="my photo" width ="30%" />
+    <h1>This is a heading 1 </h1>
+    <p>this is a para </p>
+
+    <Netflixseries></Netflixseries>
+    <Netflixseries></Netflixseries>
+    <Netflixseries></Netflixseries>
+    <Netflixseries></Netflixseries>
+    
+    </React.Fragment>
+  ) }
+
+  // return ek he kr skte h esliye div ke andr rakh diye 
+  // or jisko khud ka closeing tag nhi hoota uisko close krna prega 
+
+
+  // power of component
+  //  netflix ka phla letter captial ha N MTLB component hua
+
+  // KYA KIYE HAI just like funcn call hua ha 
+  // jo kaam ko repeat karna tha usko compnent bana kr
+  // baar baar call krdiye bs
+    const Netflixseries=()=>{
+      return (
+        <div>
+    <img src="IMAGE 2024-06-12 13:58:22.jpg" alt="my photo" width ="30%" />
+    <h1>This is a heading 1 </h1>
+    <p>this is a para </p>
+    
+    </div>
+
+      );
+    }
+
+
+/*
 export const App =()=>{
   return (<div>
 
@@ -29,6 +73,7 @@ export const App =()=>{
     <img src="IMAGE 2024-06-12 13:58:22.jpg" alt="my photo" width ="30%" />
     <h1>This is a heading 1 </h1>
     <p>this is a para </p>
+    */
     
     </div>
 
